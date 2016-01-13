54e562cc7b77fa631f5ee252ac41f78d0990bcf8
Victor <victor.suarez@auratecnologica.com>
2016-01-13 19:47:03 +0100

    subiendo APIKEY

--- src/main/java/es/ual/itsi2016/pfcmini/dolibar/DolibarConnector.java
+++ src/main/java/es/ual/itsi2016/pfcmini/dolibar/DolibarConnector.java
@@ -12,16 +12,34 @@ import org.dolibarr.www.ns.holders.ResultHolder;
 
 public class DolibarConnector {
 
-	private String APIKEY="";
-	
+	private String APIKEY = "5f0e8977e74a3203a9e9d301b4067270";
+
 	private Authentication autenticate;
-	
-	public DolibarConnector(String login, String password){
-		autenticate= new Authentication(APIKEY,"",login,password,"");
-		
+
+	public DolibarConnector(String login, String password) {
+		autenticate = new Authentication(APIKEY, "", login, password, "");
+
 	}
-	
-	public Boolean CreateClient(String ref, String email, String direccion){
+
+	public Boolean DeleteClient(String ref) {
+		WebServicesDolibarrThirdPartyPortTypeProxy proxy = new WebServicesDolibarrThirdPartyPortTypeProxy();
+		StringHolder holder = new StringHolder();
+		Result result = new Result();
+		try {
+			result = proxy.deleteThirdParty(autenticate, holder, ref, "");
+			if (result.getResult_code().equals("OK")) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public Boolean CreateClient(String ref, String email, String direccion) {
 		WebServicesDolibarrThirdPartyPortTypeProxy proxy = new WebServicesDolibarrThirdPartyPortTypeProxy();
 		Thirdparty thirdparty = new Thirdparty();
 		thirdparty.setRef(ref);
@@ -33,15 +51,16 @@ public class DolibarConnector {
 		thirdparty.setClient("1");
 		ResultHolder resultado = new ResultHolder();
 		try {
-			proxy.createThirdParty(autenticate, thirdparty, resultado, new StringHolder(), new StringHolder());
-			if(resultado.value.getResult_code().equals("OK")){
+			proxy.createThirdParty(autenticate, thirdparty, resultado,
+					new StringHolder(), new StringHolder());
+			if (resultado.value.getResult_code().equals("OK")) {
 				return true;
 			}
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return false;
 	}
 }
